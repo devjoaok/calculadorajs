e3230079bb089b4ed20bc624d1bb2c08d05ce93d
João Carvalho <contato.jccarvalho@hotmail.com>
2019-08-20 20:55:01 -0300

    [ADD] Adição dos números e input com a informação do valor atual.

--- app/src/index.js
+++ app/src/index.js
@@ -1,8 +1,97 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+
+class App extends React.Component {
+    constructor (props) {
+        super(props);
+    }
+    render () {
+      return (
+        <div className="App">
+          <div>
+              <Input />
+          </div>
+          <div className="padding-top">
+              <Board />
+          </div>
+        </div>
+      )
+    }
+  }
+  //class Board extends React.Component {
+  
+class Board extends React.Component {
+
+    renderSquare(i) {
+        return (<Square value={i} />);
+    }
+    
+    render() {
+        return (
+        <div>
+            <div className="board-row">
+            {this.renderSquare('ce')}
+            {this.renderSquare('c')}
+            {this.renderSquare('<-')}
+            {this.renderSquare('/')}
+            </div>
+            <div className="board-row">
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+            {this.renderSquare(9)}
+            {this.renderSquare('x')}
+            </div>
+            <div className="board-row">
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+            {this.renderSquare(6)}
+            {this.renderSquare('-')}
+            </div>
+            <div className="board-row">
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+            {this.renderSquare(3)}
+            {this.renderSquare('+')}
+            </div>
+            <div className="board-row">
+            {this.renderSquare('+/-')}
+            {this.renderSquare(0)}
+            {this.renderSquare(',')}
+            {this.renderSquare('=')}
+            </div>
+        </div>
+        );
+    }
+    }
+
+  class Input extends React.Component {
+    constructor (props) {
+      super(props);
+      this.state = {
+        a: 0,
+        b: 0,
+        o: '+',
+        r: 0
+      }
+    }
+  
+    render () {
+      return (
+        <input type="number" />
+      )
+    };
+  }
+  
+  function Square (props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
+  }
 
 ReactDOM.render(
     <App />, 
